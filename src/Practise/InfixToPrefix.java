4e3be8b8aeed4505059921ee66c5aeecf0f0580b
ankur1610 <vora.a@husky.neu.edu>
2017-09-13 05:52:42 -0400

    infix to prefix

---
+++ src/Practise/InfixToPrefix.java
@@ -0,0 +1,20 @@
+package Practise;
+
+import java.util.Stack;
+
+public class InfixToPrefix {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String exp = "A+B*C";
+		
+		Stack<Character> expStack = new Stack<>();
+		
+		for(int i=0; i<exp.length(); i++) {
+			
+		}
+
+	}
+
+}
